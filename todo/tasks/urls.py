205dbd18e7120208ba77cfd3c7b783318c9dec41
Higor23 <higor.lemospr@gmail.com>
2020-03-04 06:35:55 -0300

    create

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('tasks/', views.helloword),
+    
     path('', views.taskList, name = 'taskList'),
     path('task/<int:id>', views.taskView, name="task-view"),
     path('newtask/', views.newTask, name="new-task"),
-    path('yourname/<str:name>', views.yourName, name='yourname')
+    
 ]

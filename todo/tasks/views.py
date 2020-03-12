7cf96d3366e581ffbd15b94db983cd06cf665bab
Higor23 <higor.lemospr@gmail.com>
2020-03-12 17:33:25 -0300

    Listando quantidade definida

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.core.paginator import Paginator #Paginação
 from django.http import HttpResponse
 
 from .models import Task
 from .forms import TaskForm
 
 def taskList(request):
-    tasks = Task.objects.all().order_by('-created_at')
+    tasks_list = Task.objects.all().order_by('-created_at')
+    paginator = Paginator(tasks_list, 3)
+
+    page = request.GET.get('page')
+
+    tasks = paginator.get_page(page)
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 def taskView(request, id):

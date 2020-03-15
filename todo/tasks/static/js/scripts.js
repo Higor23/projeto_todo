f31f875584ef55e6a6dc0f357076f67d30840eb4
Higor23 <higor.lemospr@gmail.com>
2020-03-15 16:32:55 -0300

    Validando exclusão de tarefa

--- todo/tasks/static/js/scripts.js
+++ todo/tasks/static/js/scripts.js
@@ -1,3 +1,22 @@
-$(document).ready(function(){
-    console.log('Funcionou!');
-}
+//Criando janela de confirmação antes da deleção de uma tarefa.
+$(document).ready(function() {
+    
+    var deleteBtn = $('.delete-btn');
+
+    $(deleteBtn).on('click', function(e){
+
+        e.preventDefault();     
+    
+
+    var delLink = $(this).attr('href');
+    var result = confirm('Deseja realmente excuir a tarefa?');
+
+    if(result) {
+        window.location.href = delLink;
+    }
+
+})
+
+});
+
+
